5cc298671fe726b8292bbff74280b767667590e0
kunal <kunalgk1999@gmail.com>
2020-06-23 22:06:13 +0530

    deploy: added domain name in allowed host

--- settings/production.py
+++ settings/production.py
@@ -6,3 +6,5 @@ DEBUG = True
 SECRET_KEY = os.environ['SECRET_KEY']
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+ALLOWED_HOSTS = ['treedo-rational-labs.herokuapp.com', 'treedo.wtf']
